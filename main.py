a3fa2ce27ac1a34b71e80f04400f9565f4c346d0
XiaoMu Qin <qxm2643699211@outlook.com>
2026-01-15 09:46:04 +0800

    清理

--- main.py
+++ main.py
@@ -134,3 +134,6 @@ class MyPlugin(Star):
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        if self._daily_task:
+            self._daily_task.cancel()
+        logger.info("每日推歌挑战: 定时任务已停止")
